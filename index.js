0482df42492bc08f33c17c4d6bcbe3186b23515a
Paulo Marcelo <paulomarceloa.nogueira@gmail.com>
2019-09-24 20:49:56 -0300

    Add comments

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-var http = require('http');
-var qrCode = require('qrcode');
-var speakeasy = require('speakeasy');
+var qrCode = require('qrcode'); // Lib to generate url-based QRCode
+var speakeasy = require('speakeasy'); //Lib to passcode generator
+
 var express = require('express');
 const app = express();
 
+// Create a secret passcode
 const secret = speakeasy.generateSecret({ length: 20 });
 
 var code = null;
+// Generate a QRCode Image
 qrCode.toDataURL(secret.otpauth_url, (err, image) => {
   console.log(image);
   // https://www.site24x7.com/tools/datauri-to-image.html
+  // Use this website to render a QRCode image
+  // TODO - Render QRCode on Terminal
 });
 
 setInterval(() => {
+  // Create Totp secret
   var totp = speakeasy.totp({
     secret: secret.ascii
   });
@@ -22,6 +27,8 @@ setInterval(() => {
   code = totp;
 }, 100);
 
+// Simple router to testing
+// localhost:3000/auth?token={PSSWRD}
 app.get('/auth', (req, res, next) => {
   const { token } = req.query;
   if (token === code) {
